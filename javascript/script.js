20d3ff206e29e7ffeef865e0891a78b2bc18093e
remcoj89 <remcojonk@hotmail.com>
2023-03-09 20:48:54 +0100

    link to original page && reload button

--- javascript/script.js
+++ javascript/script.js
@@ -4,39 +4,47 @@ const recipeCategory = document.getElementById('category');
 const recipeVideo = document.getElementById('video');
 const recipeDescription = document.getElementById('recipe-description');
 const newRecipeBtn = document.getElementById('btn-next')
+const originalRecipeBtn = document.getElementById('link-to-original-recipe')
 
 let apiRecipes = []
 let mealsArray = []
 
-// Creating an ingredient list
+// Revailing Elements on Scroll
+// const observer = new IntersectionObserver()
 
 
 // Update the DOM
 function updateDOM() {
-    // main Card Container
-    mealsArray.forEach((result) => {
-        recipeImage.src = `${result.strMealThumb}`;
-        recipeName.textContent = result.strMeal;
-        recipeCategory.textContent = result.strCategory;
-        recipeVideo.src = `${result.strYoutube}`;
-        recipeDescription.textContent = result.strInstructions;
-    });
+  // main Card Container
+  mealsArray.forEach((result) => {
+    recipeImage.src = `${result.strMealThumb}`;
+    recipeName.textContent = result.strMeal;
+    recipeCategory.textContent = result.strCategory;
+    recipeVideo.src = `${result.strYoutube}`;
+    recipeDescription.textContent = result.strInstructions;
+    originalRecipeBtn.href = `${result.strSource}`;
+
+  });
 }
 
 // Get Recipes from API
 async function getRecipes() {
-    const apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
-    try {
-        const response = await fetch(apiUrl);
-        apiRecipes = await response.json();
-        mealsArray = apiRecipes.meals
-        updateDOM(mealsArray);
-    } catch (error) {
-        alert(error)
-    }
+  const apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+  try {
+    const response = await fetch(apiUrl);
+    apiRecipes = await response.json();
+    mealsArray = apiRecipes.meals
+    updateDOM(mealsArray);
+    // createList(mealsArray);
+    console.log(mealsArray)
+  } catch (error) {
+    alert(error)
+  }
 }
 
-// Changing the html
-
+// Get a new Recipe
+newRecipeBtn.addEventListener('click', function(){
+  getRecipes();
+})
 //  On load
 getRecipes();
