a028a33ba6cc20b7e7218ecb037a52d3a353d5e1
remco jonk <remcojonk@hotmail.com>
2023-08-17 12:36:36 +0200

    new recipe card

--- javascript/script.js
+++ javascript/script.js
@@ -1,65 +1,63 @@
 const recipeName = document.getElementById('recipe-name');
+const recipeCategory = document.getElementById('recipe-category')
+const recipeOrigin = document.getElementById('recipe-origin')
 const recipeImage = document.getElementById('recipe-image')
-const recipeCategory = document.getElementById('category');
-const recipeVideo = document.getElementById('video');
-const recipeDescription = document.getElementById('recipe-description');
-const newRecipeBtn = document.getElementById('btn-next')
-const originalRecipeBtn = document.getElementById('link-to-original-recipe')
+const recipeInstructions = document.getElementById('recipe-instructions')
+const recipeInstructionsVideo = document.getElementById('recipe-instructions--video')
+const recipeIngredientList = document.getElementById('recipe-ingredient-list')
+
+recipeName.textContent = "Pizza"
+recipeCategory.textContent = "Italien"
+recipeOrigin.textContent = "Italy"
+
+
 
 let apiRecipes = []
 let mealsArray = []
 
-// Update the DOM
-function updateDOM() {
-  // main Card Container
-  mealsArray.forEach((result) => {
-    recipeImage.src = `${result.strMealThumb}`;
-    recipeName.textContent = result.strMeal;
-    recipeCategory.textContent = result.strCategory;
-    // recipeVideo.src = `${result.strYoutube}`;
-    recipeDescription.textContent = result.strInstructions;
-    originalRecipeBtn.href = `${result.strSource}`;
-  });
+function showRecipevideo() {}
+
+function updateDom() {
+  mealsArray.map((meal) => {
+    console.log(meal)
+    recipeName.textContent = meal.strMeal
+    recipeCategory.textContent = meal.strCategory
+    recipeOrigin.textContent = meal.strArea
+    recipeImage.src = meal.strMealThumb
+    recipeInstructions.textContent = meal.strInstructions
+    if(meal.strYoutube.length >= 4) {
+      recipeInstructionsVideo.style.display = "block"
+      recipeInstructionsVideo.setAttribute('href' , `${meal.strYoutube}`)
+    }
+    for(let i = 1; i<= 100; i++ ){
+      const ingredient = meal[`strIngredient${i}`]
+      const measure = meal[`strMeasure${i}`]
+
+      if (ingredient && ingredient.length >0 && measure && measure.length > 0) {
+        const listItem = document.createElement("li");
+        listItem.style.listStyle = "none"
+        const listLine = document.createElement("hr");
+        listItem.textContent = `${measure} ${ingredient}`;
+        recipeIngredientList.appendChild(listItem);
+        recipeIngredientList.appendChild(listLine);
+      }
+    }
+  })
 }
 
-// Revailing Elements on Scroll
-const allSections = document.querySelectorAll('.section');
 
-const revealSection = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section-hidden')
-  observer.unobserve(entry.target);
-};
-
-const sectionObserver = new IntersectionObserver (revealSection, {
-  root: null,
-  treshold: 0.15,
-});
-
-allSections.forEach(function(section){
-  sectionObserver.observe(section);
-  section.classList.add('section-hidden')
-});
-
-// Get Recipes from API
 async function getRecipes() {
   const apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl)
     apiRecipes = await response.json();
-    mealsArray = apiRecipes.meals
-    updateDOM(mealsArray);
+    mealsArray = apiRecipes.meals;
+    updateDom();
   } catch (error) {
     alert(error)
   }
 }
 
-// Get a new Recipe
-newRecipeBtn.addEventListener('click', function(){
-  window.scrollTo(0,0);
-  window.location.reload()
-})
+recipeInstructionsVideo.addEventListener('click', showRecipevideo)
 
-//  On load
 getRecipes();
